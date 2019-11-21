5e658af963f799489b5481f2664a859159bd1994
dugusiqing <850902378@qq.com>
2019-11-21 11:28:33 +0800

    Update train.sh
    修复模型以及日志文件路径报错

--- caffe/train.sh
+++ caffe/train.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+mkdir -p models/MobileNet_v2_tsr/logs
 build/tools/caffe train --solver=examples/MobileNet_v2_tsr/solver.prototxt --model=models/MobileNet_v2_tsr/mobilenet_v2.caffemodel --gpu all 2>&1|tee  models/MobileNet_v2_tsr/logs/MobileNet_v2_tsr.log
 #####--snapshot=models/MobileNet_v/MobileNet_v2_SE_iter_440000.solverstate 
 
